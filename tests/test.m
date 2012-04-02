4a95bbeb03615d57cf9cb2fd5771314a9286d6bd
Manish Verma <manish.verma79@gmail.com>
2012-04-02 23:08:46 +0100

    Added a new help message

--- tests/test.m
+++ tests/test.m
@@ -1,7 +1,11 @@
 // RUN: cbrowse --help | FileCheck %s
 
 // CHECK: Usage: cbrowse [options] <cmd> <cmd_args> 
-// CHECK: Help...
+// CHECK: available commands are: 
+// CHECK:   build: builds the index database 
+// CHECK:   find-def: find the definition 
+// CHECK:   find-ref: find all references to the symbol 
+
 
 //CHECK: Options:
 //CHECK:  -h, --help         show this help message and exit
